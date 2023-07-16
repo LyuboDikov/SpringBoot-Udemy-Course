b3b11dd952ba53e22ea6a0aa6c47cf2bfd5714f1
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-16 12:11:25 +0300

    Made some changes on the logic and html.

--- src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
@@ -6,9 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class SuperstoreController {
@@ -26,14 +29,21 @@ public class SuperstoreController {
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item) {
+    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
         int index = getIndexFromId(item.getId());
+
+        String status = Constants.SUCCESS_STATUS;
 
         if (index == Constants.NOT_FOUND) {
             items.add(item);
-        } else {
+        } else if (within5Days(item.getDate(), items.get(index).getDate())){
             items.set(index, item);
+        } else {
+            status = Constants.FAILED_STATUS;
         }
+
+        redirectAttributes.addFlashAttribute("status", status);
+
         return "redirect:/inventory";
     }
 
@@ -48,5 +58,10 @@ public class SuperstoreController {
             if (items.get(i).getId().equals(id)) return i;
         }
         return Constants.NOT_FOUND;
+    }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
     }
 }
