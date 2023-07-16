827ad17118290e48a81f5df8f90c83aac244c947
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-16 11:32:50 +0300

    Initial commit

---
+++ src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
@@ -0,0 +1,52 @@
+package com.ltp.globalsuperstore;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SuperstoreController {
+
+    List<Item> items = new ArrayList<>();
+
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String id){
+
+        int index = getIndexFromId(id);
+
+        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
+        model.addAttribute("categories", Constants.CATEGORIES);
+        return "form";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        int index = getIndexFromId(item.getId());
+
+        if (index == Constants.NOT_FOUND) {
+            items.add(item);
+        } else {
+            items.set(index, item);
+        }
+        return "redirect:/inventory";
+    }
+
+    @GetMapping("/inventory")
+    public String getInventory(Model model){
+        model.addAttribute("items", items);
+        return "inventory";
+    }
+
+    public int getIndexFromId(String id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(id)) return i;
+        }
+        return Constants.NOT_FOUND;
+    }
+}
